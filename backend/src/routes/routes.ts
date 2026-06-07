9e32faee6c7afeb8809455c42fd8ab683f21ca6c
AlvaroNegromonte <acn3@cin.ufpe.br>
2026-06-07 02:25:17 +0000

    fix: adjust login routes to api prefix

--- backend/src/routes/routes.ts
+++ backend/src/routes/routes.ts
@@ -1,17 +1,14 @@
-import { Router } from 'express';
-import { userLoginRouter } from './user_login-routes';
-import { register, googleLogin, deleteMe, checkAuthToken } from '../controllers/authController';
+import { Router } from "express";
+import { register, googleLogin, deleteMe, checkAuthToken } from "../controllers/authController";
+import { userLoginRouter } from "./user_login-routes";
 
 const router = Router();
 
-// Rotas de Autenticação
-router.post('/register', register);
-router.post('/auth/google', googleLogin);
+router.post("/register", register);
+router.post("/auth/google", googleLogin);
 
 router.use(userLoginRouter);
 
-// Rotas de Usuário
-router.delete('/users/me', checkAuthToken, deleteMe);
-router.delete('/users/:id', checkAuthToken, deleteMe);
+router.delete("/users/me", checkAuthToken, deleteMe);
 
 export default router;
